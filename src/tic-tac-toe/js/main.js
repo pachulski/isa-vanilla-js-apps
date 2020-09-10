b43f1e3fc32990015e0b0918fc31085a57d75a7a
Radek Pachulski <radoslaw.pachulski@gmail.com>
2020-09-10 19:41:28 +0200

    tic-tac-toe: WIP

---
+++ src/tic-tac-toe/js/main.js
@@ -0,0 +1,17 @@
+// Elements
+
+const restartButton = document.querySelector('.restart');
+const playerX = document.querySelector('.playerX');
+const resultX = document.querySelector('.resultX');
+const playerO = document.querySelector('.playerO');
+const resultO = document.querySelector('.resultO');
+
+// Functions
+
+function start() {
+
+}
+
+// Event Listeners
+
+restartButton.addEventListener('click', start);
