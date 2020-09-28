e527a8dd9dfa7332c036ef5731035fbafa2df9ed
Radek Pachulski <radoslaw.pachulski@gmail.com>
2020-09-28 21:36:50 +0200

    breakout-2d: mouse controls added

--- src/breakout-2d/js/main.js
+++ src/breakout-2d/js/main.js
@@ -124,16 +124,10 @@ function draw() {
     }
   }
 
-  if (leftPressed) {
+  if (leftPressed && paddleX > 0) {
     paddleX -= 7;
-    if (paddleX < 0) {
-      paddleX = 0;
-    }
-  } else if (rightPressed) {
+  } else if (rightPressed && paddleX < canvas.width - paddleWidth) {
     paddleX += 7;
-    if (paddleX + paddleWidth > canvas.width) {
-      paddleX = canvas.width - paddleWidth;
-    }
   }
 }
 
@@ -153,7 +147,15 @@ function keyUpHandler(e) {
   }
 }
 
+const mouseMoveHandler = (e) => {
+  const relativeX = e.clientX - canvas.offsetLeft;
+  if (relativeX > 0 && relativeX < canvas.width) {
+    paddleX = relativeX - paddleWidth / 2;
+  }
+};
+
 document.addEventListener('keydown', keyDownHandler, false);
 document.addEventListener('keyup', keyUpHandler, false);
+document.addEventListener('mousemove', mouseMoveHandler, false);
 
 const interval = setInterval(draw, 10);
